041cc30cdcfc12735696a59f1e4578b0589e5cdf
profeHudsonCosta <hudson_costa@uvanet.br>
2023-09-10 14:48:50 -0300

    "Atualização"

--- heranca_multipla.py
+++ heranca_multipla.py
@@ -16,13 +16,18 @@ class Ave(Animal):
         self.cor_do_bico = cor_do_bico
 
 class Gato(Mamifero):
-    pass
+    def __init__(self, cor_do_pelo, raca, **kw):
+        super().__init__(cor_do_pelo, **kw)
+        self.raca = raca
 
 class Orinitorrinco(Mamifero, Ave):
-    pass
+    def __init__(self, cor_do_bico, cor_do_pelo, nro_de_patas):
+
+        super().__init__(cor_do_pelo=cor_do_pelo, 
+                         cor_do_bico = cor_do_bico, nro_de_patas = nro_de_patas)
 
 
-gato = Gato(nro_de_patas = 4, cor_do_pelo="frajola")
+gato = Gato(nro_de_patas = 4, cor_do_pelo="frajola", raca="Não definida")
 print("Objeto: ", gato)
 
 orinitorrinco = Orinitorrinco(nro_de_patas=2, cor_do_pelo="vermelho", cor_do_bico="laranja")
